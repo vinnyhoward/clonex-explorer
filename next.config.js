169f926efd9b08d9495aa10a631fd012f721609d
Vincent Howard <vincenguyenhoward@gmail.com>
2023-12-01 23:54:49 -0700

    rendered grid of items from endpoint.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["clonex-assets.rtfkt.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,89 @@
 "use client";
 
+import styled from "styled-components";
+import Image from "next/image";
+import { gql } from "@apollo/client";
+import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+`;
+
+const GridItem = styled.div`
+  background-color: #f0f0f0;
+  position: relative;
+  width: 100%;
+
+  &::before {
+    content: "";
+    display: block;
+    padding-top: 100%;
+  }
+
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+type TokenMetadata = {
+  image: string;
+};
+
+type Token = {
+  id: string;
+  tokenId: string;
+  metadata: TokenMetadata;
+};
+
+type QueryData = {
+  tokens: Token[];
+};
+
+const query = gql`
+  {
+    tokens(first: 42) {
+      id
+      tokenId
+      metadata {
+        image
+      }
+    }
+  }
+`;
+
 export default function Page() {
+  const { data } = useSuspenseQuery(query);
+  const typedData = data as QueryData;
+  console.log(typedData);
+
+  const renderGridItem = () => {
+    return typedData.tokens.map((token) => {
+      return (
+        <GridItem key={token.id}>
+          <div className="content">
+            <Image
+              src={token.metadata.image}
+              alt={`CloneX#${token.tokenId}`}
+              width={500}
+              height={500}
+            />
+          </div>
+        </GridItem>
+      );
+    });
+  };
+
   return (
     <main>
-      <div>Home Pages</div>
+      <Grid>{renderGridItem()}</Grid>
     </main>
   );
 }
