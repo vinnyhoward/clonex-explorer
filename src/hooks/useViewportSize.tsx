518e64f6259917d85f6233da8f2f76db32524e42
Vincent Howard <vincenguyenhoward@gmail.com>
2024-01-10 21:46:49 -0700

    remove burnt package and added sharp package to write a script to resize image and host on google

--- src/hooks/useViewportSize.tsx
+++ src/hooks/useViewportSize.tsx
@@ -6,7 +6,6 @@ interface ViewportSize {
 }
 
 export const useViewportSize = (): ViewportSize => {
-  // Initialize with undefined or null values
   const [size, setSize] = useState<ViewportSize>({
     width: undefined,
     height: undefined,
@@ -14,7 +13,6 @@ export const useViewportSize = (): ViewportSize => {
 
   useEffect(() => {
     if (typeof window === "undefined") return;
-    
     const updateSize = () => {
       setSize({ width: window.innerWidth, height: window.innerHeight });
     };
